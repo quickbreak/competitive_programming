b6b576400b998b1c435ad6b013a3e1d83a53d1ef
quickbreak <michaelzapryagaev@gmail.com>
2025-04-12 14:54:42 +0300

    Create 1800D - Удали два символа.cpp

---
+++ Решения/Codeforces/Задачи по номеру (раздел Архив)/1800D - Удали два символа.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main() {
+	int t; cin >> t;
+	while (t-- > 0) {
+		int n; cin >> n;
+		string s; cin >> s;
+		int ans = n - 1;
+		for (int i = 1; i < n - 1; ++i) {
+			if (s[i + 1] == s[i - 1])
+				--ans;
+		}
+		cout << ans << '\n';
+	}
+}
